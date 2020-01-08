e36e92fb2616ede6ba676f9b75b373eb5ee9c866
Jeff Swisher <jefftswisher@gmail.com>
2020-01-08 18:45:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,12 +3,14 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2] 
   
   def change 
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.string :size 
-      t.string :image_url  
+      t.integer :employees
+      t.boolean :in_business
+      t.datetime :open
+      t.datetime :closed
     end 
   end 
   
