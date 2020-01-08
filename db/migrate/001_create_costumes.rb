105137c0e0048238025b7bccc601b19c246fecf4
Jeff Swisher <jefftswisher@gmail.com>
2020-01-08 18:32:21 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,8 +16,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price 
       t.string :size 
       t.string :image_url  
-      t.string :created_at 
-      t.string :updated_at
+      t.datetime :created_at 
+      t.datetime :updated_at
     end 
   end 
   
